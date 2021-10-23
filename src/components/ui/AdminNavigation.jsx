bba25286a4658f6c7d93ca3308ea7b60b8a45106
sammwaura <meshsami27@gmail.com>
2021-10-23 13:10:23 +0300

    modify and create new files

--- src/components/ui/AdminNavigation.jsx
+++ src/components/ui/AdminNavigation.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
-const AdminNavigation = () =>{
-    return(
-        <div>
-            <h1>
-                AdminNavigation
-            </h1>
-        </div>
-    )
-}
-export default  AdminNavigation
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import UserAvatar from 'views/account/components/UserAvatar';
+import CircularProgress from './CircularProgress';
+
+ 
+const AdminNavigation = () => {
+  const { isAuthenticating, profile } = useSelector(state => ({
+    isAuthenticating: state.app.isAuthenticating,
+    profile: state.profile
+  }));
+
+  return (
+    <nav className="navigation navigation-admin">
+      <div className="logo">
+        <Link to='/admin/dashboard'>
+          <h2>ADMIN PANEL</h2>
+        </Link>
+      </div>
+      <ul className="navigation-menu">
+        <li className="navigation-menu-item">
+          <UserAvatar 
+              isAuthenticating={isAuthenticating} 
+              profile={profile} 
+          />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default AdminNavigation;
--- src/components/ui/AdminSidePanel.jsx
+++ src/components/ui/AdminSidePanel.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-const SideNavigation = () =>{
-    return(
-        <div>
-            <h1>
-                Side Navigation
-            </h1>
+import { NavLink } from 'react-router-dom'; 
+
+
+const SideNavigation = () => {
+  return (
+    <aside className="sidenavigation">
+      <div className="sidenavigation-wrapper">
+        <div className="sidenavigation-item">
+          <NavLink 
+              activeClassName="sidenavigation-menu-active"
+              className="sidenavigation-menu"
+              to='/admin/products'
+          >
+            Products
+          </NavLink>
         </div>
-    )
-}
-export default  SideNavigation
+        <div className="sidenavigation-item">
+          <a className="sidenavigation-menu"></a>
+          <NavLink 
+                activeClassName="sidenavigation-menu-active"
+                className="sidenavigation-menu"
+                to="/dashboard/users"
+            >
+              Orders
+            </NavLink>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default SideNavigation;
